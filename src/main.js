a768a9f7c8724591cbbe71466f89d73e0c4d5904
maenoWin-codes <cacortesl01@gmail.com>
2022-10-22 18:07:44 -0500

    Se agrega código js para realizar las primeras consultas a la API, cambios alhtml y css

--- src/main.js
+++ src/main.js
@@ -1,2 +1,21 @@
 
-console.log(process.env.REACT_APP_API_KEY_API_KEY);
+
+async function trendingMoView(){
+    const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key="+API_KEY);
+    const data = await res.json();
+    console.log("data", data);
+
+    const trengingContainerCards = document.querySelector(".trending__cards-container");
+    const fragment = new DocumentFragment();
+    for (const item of data.results) {
+        console.log(item.backdrop_path);
+        /* const movieImg = document.createElement("img");
+        movieImg.src= item.poster_path;
+        fragment.appendChild(movieImg); */
+    }
+    /* trengingContainerCards.append(fragment); */
+    
+
+}
+
+trendingMoView();
