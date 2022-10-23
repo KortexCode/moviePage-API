dd78f09420f9fc503240dbe374987f6b0a1cd2fa
maenoWin-codes <cacortesl01@gmail.com>
2022-10-22 21:52:40 -0500

    Se realiza consulta a la API, se muestran imágenes de películas en tendencia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 
 
-async function trendingMoView(){
+async function trendingMovieView(){
     const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key="+API_KEY);
     const data = await res.json();
     console.log("data", data);
@@ -8,14 +8,36 @@ async function trendingMoView(){
     const trengingContainerCards = document.querySelector(".trending__cards-container");
     const fragment = new DocumentFragment();
     for (const item of data.results) {
-        console.log(item.backdrop_path);
-        /* const movieImg = document.createElement("img");
-        movieImg.src= item.poster_path;
-        fragment.appendChild(movieImg); */
+        
+        const movieImg = document.createElement("img");
+        movieImg.setAttribute("alt", item.original_title);
+        movieImg.src= "https://image.tmdb.org/t/p/w500"+item.poster_path;
+        fragment.appendChild(movieImg);
     }
-    /* trengingContainerCards.append(fragment); */
+    trengingContainerCards.append(fragment);
     
 
 }
 
-trendingMoView();
+trendingMovieView();
+/* 
+async function popularMovieView(){
+    const res = await fetch("https://api.themoviedb.org/3/popular/movie/day?api_key="+API_KEY);
+    const data = await res.json();
+    console.log("data", data);
+
+    const popularContainerCards = document.querySelector(".popular__cards-container");
+    const fragment = new DocumentFragment();
+    for (const item of data.results) {
+        
+        const movieImg = document.createElement("img");
+        movieImg.setAttribute("alt", item.original_title);
+        movieImg.src= "https://image.tmdb.org/t/p/w500"+item.poster_path;
+        fragment.appendChild(movieImg);
+    }
+    popularContainerCards.append(fragment);
+    
+
+}
+
+popularMovieView(); */
