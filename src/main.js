b1e198e63d4107ecc8c5e3f7d6dd5c2ac40fc038
maenoWin-codes <cacortesl01@gmail.com>
2022-10-24 16:40:30 -0500

    Se comienza la construcción de la vista por categoría.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-
+const bodyBackground = document.querySelector("body");
 
 async function trendingMovieView(){
     const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key="+API_KEY);
@@ -14,6 +14,27 @@ async function trendingMovieView(){
         movieImg.src= "https://image.tmdb.org/t/p/w500"+item.poster_path;
         fragment.appendChild(movieImg);
     }
+
+    trengingContainerCards.append(fragment);
+    
+
+}
+
+async function categoryMovieView(){
+    const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key="+API_KEY);
+    const data = await res.json();
+    console.log("data", data);
+
+    const trengingContainerCards = document.querySelector(".trending__cards-container");
+    const fragment = new DocumentFragment();
+    for (const item of data.results) {
+        
+        const movieImg = document.createElement("img");
+        movieImg.setAttribute("alt", item.original_title);
+        movieImg.src= "https://image.tmdb.org/t/p/w500"+item.poster_path;
+        fragment.appendChild(movieImg);
+    }
+    
     trengingContainerCards.append(fragment);
     
 
