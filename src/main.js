34d1c45fcdaac190cb0e8df92507be1bfe6e71c2
maenoWin-codes <cacortesl01@gmail.com>
2022-10-25 11:20:52 -0500

    Se agregan categorías consultadas por la API al menú mobile y desktop de la barra de navegación. Cambios al css, html y js.

--- src/main.js
+++ src/main.js
@@ -19,28 +19,73 @@ async function trendingMovieView(){
     
 
 }
-
+//Cargar las categorías en el menú Desktop, Desktop dropdown button.
 async function categoryMovieView(){
-    const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key="+API_KEY);
+   /*  Se realiza consulta por genéros de películas */
+    const res = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key="+API_KEY);
     const data = await res.json();
     console.log("data", data);
 
-    const trengingContainerCards = document.querySelector(".trending__cards-container");
-    const fragment = new DocumentFragment();
-    for (const item of data.results) {
-        
-        const movieImg = document.createElement("img");
-        movieImg.setAttribute("alt", item.original_title);
-        movieImg.src= "https://image.tmdb.org/t/p/w500"+item.poster_path;
-        fragment.appendChild(movieImg);
+    const categoryDesktop = document.getElementById("category-desktop-menu");
+    const categoryMobile = document.getElementById("category-mobile-menu");
+    const dropItemsContainer = document.getElementById("dropdown-items-container");
+    const dropdownMobileItems = document.getElementById("dropdown-mobile-items");
+
+    for (const item of data.genres) {
+        //Se crean las etiquetas y se agregan atributos y clases
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        const i = document.createElement("i");
+        //Se agregan algunas clases de bootstrap y también atributos 
+        a.classList.add("dropdown-item", "d-flex", "align-items-center");
+        a.setAttribute("href", "#");
+        i.classList.add("fa-solid", "fa-circle", "text-dark");
+        a.append(i);
+        a.append(item.name);
+        li.append(a);
+        dropItemsContainer.append(li);
+       
     }
-    
-    trengingContainerCards.append(fragment);
+    for (const item of data.genres) {
+        //Se crean las etiquetas y se agregan atributos y clases
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        const i = document.createElement("i");
+        
+        //Se agregan algunas clases de bootstrap y también atributos 
+        a.classList.add("nav-link", "d-flex", "align-items-center");
+        a.setAttribute("href", "#");
+        i.classList.add("fa-solid", "fa-circle", "text-dark");
+        li.classList.add("nav-item", "d-md-none");
+        a.append(i);
+        a.append(item.name);
+        li.append(a);
+        dropdownMobileItems.append(li);
+       
+    }
+    categoryDesktop.appendChild(dropItemsContainer);
+    categoryMobile.append(dropdownMobileItems);
+  
+       
+       
     
 
 }
 
-trendingMovieView();
+categoryMovieView();
+/* trendingMovieView(); */
+
+
+
+
+
+
+
+
+
+
+
+
 /* 
 async function popularMovieView(){
     const res = await fetch("https://api.themoviedb.org/3/popular/movie/day?api_key="+API_KEY);
