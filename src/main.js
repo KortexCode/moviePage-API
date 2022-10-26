1014e8becbbd40c92158a5688dd3fd557ed3c73d
maenoWin-codes <cacortesl01@gmail.com>
2022-10-26 15:41:33 -0500

    Se agrega archivo node.js para obtener los elementos generadores de acciones. Se habilita la sección de categorías en modo display none

--- src/main.js
+++ src/main.js
@@ -17,17 +17,17 @@ async function trendingMovieView(){
         const data = await res.json();
         console.log("data", data);
     
-        const trengingContainerCards = document.querySelector(".trending__cards-container");
+        const trendingCardsContainer = document.querySelector(".trending__cards-container");
         const fragment = new DocumentFragment();
         for (const item of data.results) {
-            
+
             const movieImg = document.createElement("img");
             movieImg.setAttribute("alt", item.original_title);
             movieImg.src= "https://image.tmdb.org/t/p/w500"+item.poster_path;
             fragment.appendChild(movieImg);
         }
     
-        trengingContainerCards.append(fragment);
+        trendingCardsContainer.append(fragment);
     }
     catch(error){
         console.log("Sorry"+error);
@@ -37,66 +37,61 @@ async function trendingMovieView(){
 }
 
 //Cargar las categorías en el menú Desktop, Desktop dropdown button.
-async function categoryMovieView(){
-   /*  Se realiza consulta por genéros de películas */
-   /*  const res = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key="+API_KEY);
-    const data = await res.json();
-    console.log("data", data); */
- try{
-     const {data} = await api("genre/movie/list");//El resultado no necesita de json()
-     
-     const categoryDesktop = document.getElementById("category-desktop-menu");
-     const categoryMobile = document.getElementById("category-mobile-menu");
-     const dropItemsContainer = document.getElementById("dropdown-items-container");
-     const dropdownMobileItems = document.getElementById("dropdown-mobile-items");
- 
-     for (const item of data.genres) {
-         //Se crean las etiquetas y se agregan atributos y clases
-         const li = document.createElement("li");
-         const a = document.createElement("a");
-         const i = document.createElement("i");
-         //Se agregan algunas clases de bootstrap y también atributos 
-         a.classList.add("dropdown-item", "d-flex", "align-items-center");
-         a.setAttribute("href", "#");
-         i.classList.add("fa-solid", "fa-circle", "text-dark");
-         a.append(i);
-         a.append(item.name);
-         li.append(a);
-         dropItemsContainer.append(li);
-        
-     }
-     for (const item of data.genres) {
-         //Se crean las etiquetas y se agregan atributos y clases
-         const li = document.createElement("li");
-         const a = document.createElement("a");
-         const i = document.createElement("i");
-         
-         //Se agregan algunas clases de bootstrap y también atributos 
-         a.classList.add("nav-link", "d-flex", "align-items-center");
-         a.setAttribute("href", "#");
-         i.classList.add("fa-solid", "fa-circle", "text-dark");
-         li.classList.add("nav-item", "d-md-none");
-         a.append(i);
-         a.append(item.name);
-         li.append(a);
-         dropdownMobileItems.append(li);
-        
-     }
-     categoryDesktop.appendChild(dropItemsContainer);
-     categoryMobile.append(dropdownMobileItems);
+async function categoryMoviePreview(){
+    /*  Se realiza consulta por genéros de películas usando fetch*/
+    /*  const res = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key="+API_KEY);
+     const data = await res.json();*/
+    try{
+        const {data} = await api("genre/movie/list");//El resultado no necesita de json()
 
- }
- catch(error){
-    console.log("sorry "+ error);
- }
-       
-       
-    
+        const categoryDesktop = document.getElementById("category-desktop-menu");
+        const categoryMobile = document.getElementById("category-mobile-menu");
+        const dropItemsContainer = document.getElementById("dropdown-items-container");
+        const dropdownMobileItems = document.getElementById("dropdown-mobile-items");
 
+        for (const item of data.genres) {
+            //Se crean las etiquetas y se agregan atributos y clases
+            const li = document.createElement("li");
+            const a = document.createElement("a");
+            const i = document.createElement("i");
+            //Se agregan algunas clases de bootstrap y también atributos 
+            a.classList.add("dropdown-item", "d-flex", "align-items-center");
+            a.setAttribute("href", "#");
+            i.classList.add("fa-solid", "fa-circle", "text-dark");
+            a.append(i);
+            a.append(item.name);
+            li.append(a);
+            dropItemsContainer.append(li);
+        
+        }
+        for (const item of data.genres) {
+            //Se crean las etiquetas y se agregan atributos y clases
+            const li = document.createElement("li");
+            const a = document.createElement("a");
+            const i = document.createElement("i");
+
+            //Se agregan algunas clases de bootstrap y también atributos 
+            a.classList.add("nav-link", "d-flex", "align-items-center");
+            a.setAttribute("href", "#");
+            i.classList.add("fa-solid", "fa-circle", "text-dark");
+            li.classList.add("nav-item", "d-md-none");
+            a.append(i);
+            a.append(item.name);
+            li.append(a);
+            dropdownMobileItems.append(li);
+        
+        }
+        categoryDesktop.appendChild(dropItemsContainer);
+        categoryMobile.append(dropdownMobileItems); 
+    }
+    catch(error){
+        console.log("sorry "+ error);
+    }
+       
 }
 
-categoryMovieView();
-/* trendingMovieView(); */
+categoryMoviePreview();
+
 
 
 
--- src/navigation.js
+++ src/navigation.js
@@ -1,21 +1,30 @@
+/* Aquí se reciben los eventos que generan un cambio de vista en el DOM */
+location.hash = "#trends";
+/* Se decta el cambio en el hash y la primera carga del Dom al iniciar la aplicación */
 window.addEventListener("DOMContentLoaded", navigator, false);
 window.addEventListener("hashchange", navigator, false);
 
 function navigator(){
 
     location.hash.startsWith("#trends") ? homePage() :  location.hash.startsWith("#search=") ? searchPage()
-    : location.hash.startsWith("#movie") ?  moviePage() : location.hash.startsWith("#category") ? categoryhPage() : homePage();
+    : location.hash.startsWith("#movie") ?  moviePage() : location.hash.startsWith("#category") ? categoryhPage() 
+    : homePage();
 }
 
 function homePage(){
     console.log("Home");
+    categories.classList.add("d-none");
+    trendingMovieView();
 }
-function searchPage(){
+function categoryhPage(){
+    cat
+    console.log("Category");
+    
+}
+/* function searchPage(){
     console.log("Search");
 }
 function moviePage(){
-    console.log("Search");
+    console.log("Movie");
 }
-function categoryhPage(){
-    console.log("Search");
-}
+ */
